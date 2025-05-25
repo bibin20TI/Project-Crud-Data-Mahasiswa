54f4251ae56d6898009f2b0f9fe0e009d355fabd
bibin20TI <91010667+bibin20TI@users.noreply.github.com>
2025-05-25 19:29:35 +0700

    menammbahkan method SecuritHelper

---
+++ HashHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Crud
+{
+    class HashHelper
+    {//  
+        public static string hashPassword(string password)
+        {   // ini merupakan method static yang akan 
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                Byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                return BitConverter.ToString(bytes).Replace("-", "").ToLower();
+
+            }
+        }
+
+    }
+}
